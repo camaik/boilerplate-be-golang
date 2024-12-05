a021b40a769fcb252c72273640797a21c8d397a4
Rahmat Hidayat <rahdyt@gmail.com>
2024-12-06 04:28:12 +0700

    jwt auth & validate token

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"karyaoneplus/auth"
 	"karyaoneplus/handler"
 	"karyaoneplus/user"
@@ -22,6 +23,19 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
+
+	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyfQ.o_gE9qVP0kL6inwLg7ndxR_OpeDloDX727aLX_dU9Jg")
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println("Error")
+		fmt.Println("Error")
+	}
+
+	if token.Valid {
+		fmt.Println("Valid")
+	} else {
+		fmt.Println("Invalid")
+	}
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
